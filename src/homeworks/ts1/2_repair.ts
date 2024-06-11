6ed7f9a92685853058d41714337388973ff1afb7
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2024-06-11 22:40:02 +0300

    feat(2_repair.ts): HW 1 part 2

--- src/homeworks/ts1/2_repair.ts
+++ src/homeworks/ts1/2_repair.ts
@@ -3,45 +3,58 @@
  * */
 
 // // Мы это не проходили, но по тексту ошибки можно понять, как это починить
-// export const getFakeApi = async (): void => {
-//   const result = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => response.json());
-//   console.log(result);
-// };
-//
-// // Мы это не проходили, но по тексту ошибки можно понять, как это починить
-// export class SomeClass {
-//   constructor() {
-//     this.set = new Set([1]);
-//     this.channel = new BroadcastChannel('test-broadcast-channel');
-//   }
-// }
-//
-// export type Data = {
-//   type: 'Money' | 'Percent';
-//   value: DataValue;
-// };
-//
-// export type DataValue = Money | Percent;
-//
-// export type Money = {
-//   currency: string;
-//   amount: number;
-// };
-//
-// export type Percent = {
-//   percent: number;
-// };
-//
-// // Здесь, возможно, нужно использовать as, возможно в switch передавать немного по-другому
-// const getDataAmount = (data: Data): number => {
-//   switch (data.type) {
-//     case 'Money':
-//       return data.value.amount;
-//
-//     default: {
-//       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//       const unhandled: never = data; // здесь, возможно, нужно использовать нечто другое. :never должен остаться
-//       throw new Error(`unknown type: ${data.type}`);
-//     }
-//   }
-// };
+export const getFakeApi = async (): Promise<void> => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/todos/1').then((response) => response.json());
+  console.log(result);
+};
+
+// Мы это не проходили, но по тексту ошибки можно понять, как это починить
+export class SomeClass {
+  set: Set<number>;
+  channel: BroadcastChannel;
+  constructor() {
+    this.set = new Set([1]);
+    this.channel = new BroadcastChannel('test-broadcast-channel');
+  }
+}
+
+enum DataType {
+  MONEY = 'Money',
+  PERCENT = 'Percent',
+}
+
+type DataMoney = {
+  type: DataType.MONEY;
+  value: Money;
+};
+
+type DataPercent = {
+  type: DataType.PERCENT;
+  value: Percent;
+};
+
+export type Data = DataMoney | DataPercent;
+
+export type Money = {
+  currency: string;
+  amount: number;
+};
+
+export type Percent = {
+  percent: number;
+};
+
+// Здесь, возможно, нужно использовать as, возможно в switch передавать немного по-другому
+const getDataAmount = (data: Data | unknown): number => {
+  switch ((data as Data).type) {
+    case DataType.MONEY:
+      return (data as DataMoney).value.amount;
+
+    default: {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const unhandled: DataPercent | unknown | never = data; // здесь, возможно, нужно использовать нечто другое. :never должен остаться
+      const errorMessage = `unknown type: ${(data as DataPercent).type ? (data as DataPercent).type : 'unknown'}`;
+      throw new Error(errorMessage);
+    }
+  }
+};
