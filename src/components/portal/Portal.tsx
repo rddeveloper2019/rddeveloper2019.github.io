92d0610dd47e6054c52e57e7168f5d783f0519cd
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2024-09-04 20:11:55 +0300

    fix(Portal): change portal parent element

--- src/components/portal/Portal.tsx
+++ src/components/portal/Portal.tsx
@@ -1,22 +1,11 @@
-import React, { ReactNode, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
 
 interface PortalProps {
   children: ReactNode;
 }
 
+const container = document.body;
 export const Portal: React.FC<PortalProps> = ({ children }) => {
-  // новый контейнер div в document.body и рендерит children
-  const [container] = useState(() => document.createElement('div'));
-
-  useEffect(() => {
-    // контейнер добавляется в body
-    document.body.appendChild(container);
-    return () => {
-      // когда размонтируется, контейнер удаляется.
-      document.body.removeChild(container);
-    };
-  }, [container]);
-
-  return ReactDOM.createPortal(children, container);
+  return createPortal(children, container);
 };
