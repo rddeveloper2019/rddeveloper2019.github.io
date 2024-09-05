fd424a3268574689aaa219ef6186d37a74206a28
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2024-09-06 01:22:26 +0300

    feat(operations-list): add infinite scroll storybook

--- src/components/operations-list/operations-list.stories.tsx
+++ src/components/operations-list/operations-list.stories.tsx
@@ -1,6 +1,10 @@
 import type { Meta } from '@storybook/react';
 import OperationsList from './index';
 import { createRandomOperations } from '../../model/utils';
+import { MainProvider } from 'src/store/provider';
+import Header from 'src/components/header/header';
+import React, { useEffect, useState } from 'react';
+import { Operation } from 'src/model/types';
 
 const meta: Meta<typeof OperationsList> = {
   title: 'Components/OperationsList',
@@ -9,9 +13,26 @@ const meta: Meta<typeof OperationsList> = {
 };
 export default meta;
 
-const operations = createRandomOperations(25);
-export const Detail = {
+const operations = createRandomOperations(10);
+export const Default = {
   args: {
     operations,
+    isInfinite: false,
   },
+};
+
+export const InfiniteScroll = () => {
+  const [count, setCount] = useState<number>(0);
+  const [operations, setOperations] = useState<Operation[]>([...createRandomOperations(10)]);
+
+  useEffect(() => {
+    if (count) {
+      setOperations((prev) => [...prev, ...createRandomOperations(5)]);
+    }
+  }, [count]);
+  return (
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
+      <OperationsList operations={operations} addMore={() => setCount(count + 1)} />
+    </div>
+  );
 };
