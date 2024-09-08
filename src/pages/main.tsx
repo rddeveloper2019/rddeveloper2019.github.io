50e4b821237a06185d555dcc71e9faf735af407f
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2024-09-08 22:39:43 +0300

    fix(mainpage): show content on auth

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { ModalControl } from 'src/components/modal-control/modal-control';
+import React, { useContext, useEffect, useState } from 'react';
 import OperationsList from '../components/operations-list';
 import { createRandomOperations } from 'src/model/utils';
 import { Operation } from '../model/types';
-import LoginForm from 'src/components/login-form/login-form';
+import { MainContext } from 'src/store/provider';
 
 export const MainPage = () => {
   const [count, setCount] = useState<number>(0);
   const [operations, setOperations] = useState<Operation[]>([...createRandomOperations(10)]);
+  const { isAuth } = useContext(MainContext);
 
   useEffect(() => {
     if (count) {
@@ -17,9 +17,11 @@ export const MainPage = () => {
 
   return (
     <div style={{ display: 'flex', padding: 20 }}>
-      <div style={{ width: '50%' }}>
-        <OperationsList operations={operations} addMore={() => setCount(count + 1)} />
-      </div>
+      {isAuth && (
+        <div style={{ width: '50%' }}>
+          <OperationsList operations={operations} addMore={() => setCount(count + 1)} />
+        </div>
+      )}
     </div>
   );
 };
