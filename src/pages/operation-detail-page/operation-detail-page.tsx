7a550ddd60275cfebffad198d4b099216caca4b7
Rid Van <fatzey2010@gmail.com>
2024-09-30 23:04:20 +0300

    fix(*): redirect to 404, redirect to detail

--- src/pages/operation-detail-page/operation-detail-page.tsx
+++ src/pages/operation-detail-page/operation-detail-page.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import styles from './operation-datail-page.module.scss';
 import OperationDetail from '../../components/operation-detail/operation-detail';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Card from '../../components/card/Card';
 import { useOperationsSelector } from '../../store/selectors';
 
 export const OperationDetailPage = () => {
   const { operations } = useOperationsSelector();
   const { state } = useLocation();
+  const navigate = useNavigate();
+
+  const operation = operations.find(({ id }) => id === state.id);
+  if (!state.id || !operation) {
+    navigate('*');
+    return;
+  }
 
   return (
     <div className={styles.page}>
